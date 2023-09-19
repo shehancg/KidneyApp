471a8c50513ee8394fa84f687525bd642d18d910
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-09-19 13:28:43 +0530

    UI dev and add donor page

---
+++ app/src/main/java/com/example/kidneyapp/AddDonorActivity.java
@@ -0,0 +1,14 @@
+package com.example.kidneyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDonorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_donor);
+    }
+}
--- app/src/main/java/com/example/kidneyapp/HomeActivity.java
+++ app/src/main/java/com/example/kidneyapp/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.kidneyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button addButton = findViewById(R.id.btn_add);
+        Button searchButton = findViewById(R.id.btn_search);
+
+        addButton.setOnClickListener(v -> {
+            // Create an Intent to open the target activity
+            Intent intent = new Intent(HomeActivity.this, AddDonorActivity.class); // Replace TargetActivity with the actual name of your target activity class
+
+            // Start the target activity
+            startActivity(intent);
+        });
+
+        searchButton.setOnClickListener(v -> {
+            // Create an Intent to open the target activity
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class); // Replace TargetActivity with the actual name of your target activity class
+
+            // Start the target activity
+            startActivity(intent);
+        });
     }
+
 }
